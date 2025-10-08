94f5f086d14432e733719c766d94c52195c40113
Frank_999 <franciscoestuardofranco2004@gmail.com>
2025-10-08 05:52:46 -0600

    Comienzo del Producto Final

---
+++ src/main/java/com/gestor/gestor_sat/GestorSatApplication.java
@@ -0,0 +1,13 @@
+package com.gestor.gestor_sat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorSatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorSatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestor/gestor_sat/GestorSatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor.gestor_sat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorSatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
